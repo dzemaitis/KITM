58241a7b960beb841efbf3b261d0d1060c7aaa38
dzemaitis <zemait@protonmail.com>
2019-09-30 22:42:29 +0300

    pirmadienis

---
+++ dotnet/pratimai/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace pratimai
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            int a;
+            a = Int32.Parse(Console.ReadLine());
+            int b;
+            b = Int32.Parse(Console.ReadLine());
+            int temp;
+            temp = a;
+            a = b;
+            b = temp;
+            Console.WriteLine(b,a);
+        }
+    }
+}
