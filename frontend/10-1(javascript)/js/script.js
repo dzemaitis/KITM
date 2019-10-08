73682c100f00be31c0e28a6376f7f86a1f8585ea
dzemaitis <zemait@protonmail.com>
2019-10-08 11:24:58 +0300

    Update script.js

--- frontend/10-1(javascript)/js/script.js
+++ frontend/10-1(javascript)/js/script.js
@@ -52,7 +52,7 @@
 var knygynas = { // galima ir [] naudoti.
 	grozines : [
 		{ISBN: 3548354, leidimoMetai: 1988, pavadinimas: "receptu knyga", puslapiai: 421, kaina: 0.99},
-		{ISBN: 7441354, leidimoMetai: 1988, pavadinimas: "haris poteris", puslapiai: 231, kaina: 5.99},
+		{ISBN: 7441354, leidimoMetai: 1988, pavadinimas: "haris poteris7", puslapiai: 231, kaina: 5.99},
 		{ISBN: 3592354, leidimoMetai: 2018, pavadinimas: "ziedu valdovdas", puslapiai: 482, kaina: 9.99},
 		{ISBN: 3321354, leidimoMetai: 2018, pavadinimas: "qwerty", puslapiai: 391, kaina: 12.99}
 	],
@@ -66,7 +66,7 @@ var knygynas = { // galima ir [] naudoti.
 		{ISBN: 3546354, leidimoMetai: 1988, pavadinimas: "haris poteris4", puslapiai: 109, kaina: 15.30},
 		{ISBN: 3541329, leidimoMetai: 1988, pavadinimas: "haris poteris1", puslapiai: 324, kaina: 9.30},
 		{ISBN: 3741354, leidimoMetai: 2018, pavadinimas: "romie", puslapiai: 221, kaina: 11.30},
-		{ISBN: 3573355, leidimoMetai: 2018, pavadinimas: "haris poteris2", puslapiai: 623, kaina: 8.30}
+		{ISBN: 3573355, leidimoMetai: 2018, pavadinimas: "haris poteris11", puslapiai: 623, kaina: 8.30}
 	]
 }
 // for (let kategorijos in knygynas){
@@ -78,8 +78,6 @@ var knygynas = { // galima ir [] naudoti.
 // 		}
 // 	}
 // }
-
-// console.log(knygynas['grozines']['grozineKnyga4']['ISBN'] < knygynas['grozines']['grozineKnyga3']['ISBN'])
 
 
 function isleista2008(){
@@ -106,3 +104,81 @@ function pigiausiaKategorijoj(){
 // isleista2008()
 // pigiausiaKategorijoj()
 
+function pavadinimuMasyvoSukurimas(){
+	let knyguPavadinimai = []
+
+	for (let kategorija in knygynas){
+		for (let knyga in knygynas[kategorija]){
+			knyguPavadinimai.push(knygynas[kategorija][knyga]['pavadinimas'])
+		}
+	}
+	knyguPavadinimai.sort()
+	for(let pavadinimas of knyguPavadinimai){
+		console.log(pavadinimas)
+	}
+}
+
+// pavadinimuMasyvoSukurimas()
+
+function Skaitytojas(vardas, pavarde, perskaitytosKnygos){ //iniciatorius
+	this.vardas = vardas; //objekto key sukuria.
+	this.pavarde = pavarde;
+	this.perskaitytosKnygos = perskaitytosKnygos
+	this.greeting = function(){
+		console.log("Hi i'm, " + vardas + " " + pavarde + " perskaitytos knygos: " + perskaitytosKnygos)
+	}
+}
+
+let skaitytojas1 = new Skaitytojas("Deividas", "Zemaitis", [1,2,3]);
+
+skaitytojas1.greeting()
+
+Skaitytojas.prototype.labas = function(){ //prototype keyword, kad sukurti metoda isoreje, o ne iniciatoriaus viduje.
+	console.log(this.vardas + " sako labas")
+}
+skaitytojas1.labas()
+
+function VipSkaitytojas(vardas, pavarde, perskaitytosKnygos, rankas){ //paveldejimas(senasis budas)
+	Skaitytojas.call(this, vardas,pavarde, perskaitytosKnygos)
+	this.rankas = rankas
+}
+
+let skaitytojas2 = new VipSkaitytojas("Marius", "Grybauskas", [1,2,3], "leitenantas")
+console.log(skaitytojas2.rankas)
+
+// ----------------SC6-------------
+
+// naujoves:
+
+console.log(`rankas: ${skaitytojas2.rankas},
+ va taip su doleriu zenklu atspauzdint`) //galima i kita eilute kelt, ir kintamuose su $, bet reik `` naudot (apostrofa)
+
+
+let [amzius, svoris, ugis] = [26, 80, 186] //kaip pythone galime sukurt. (array destructing. galima ir su objektu)
+console.log(amzius)
+
+
+function paieska(arr, value){
+	console.log(arr.includes(value))
+}
+
+paieska(['albas', 'marcius', 'giecka', 'zora', 'remyga'], 'albas') //true
+
+// atsiranda klases. Dabar taip rasosi. Jau su zodziu class:
+
+class SkaitytojasSD6{
+	constructor(vardas, pavarde, perskaitytosKnygos){
+		this.vardas = vardas; 
+		this.pavarde = pavarde;
+		this.perskaitytosKnygos = perskaitytosKnygos
+		}
+	greeting(){
+		console.log(`${this.vardas} labas!` )
+	}
+}
+
+let skaitytojas_is_sd6 = new SkaitytojasSD6('deivis', 'zemaitis', [1,2,3])
+
+skaitytojas_is_sd6.greeting()
+
+//paveldejimas 
