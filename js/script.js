064dfb75c65fda91c54ac8a59a178728482ef2e8
dzemaitis <55211085+dzemaitis@users.noreply.github.com>
2019-10-09 00:25:08 +0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,36 @@
+var arr = ["aaa", "bbb", "ccc", "ddd", "eee", "fff"]
+
+arr.push("ggg")
+arr.pop();
+
+arr.shift() //pasalina pirma. Bet letas metodas praktikoje.
+arr.unshift("000") //i pradzia
+
+arr.splice(1,2)
+
+
+
+
+
+var mokiniai = ["Jonas", "Andrius", "Marius"]
+
+// skaicius = 1
+
+// let prideti = function(vardas, skaicius){
+// 	return vardas + skaicius
+// }
+// for (let mokinys of mokiniai){
+// 	console.log(prideti(mokinys, skaicius))
+// 	skaicius += 1
+// }
+
+
+var objektai = [{kaina: 22.00, id: 3}, {kaina:7.64, id: 4}, {kaina:19.22, id:11}]
+
+
+
+objektai.sort(function(a, b){
+  return a.kaina > b.kaina;
+});
+
+console.log(typeof objektai[0].kaina)
